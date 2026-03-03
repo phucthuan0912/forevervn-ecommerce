22c6f986db2dd98eb58c85ec29a8c0390252b2d4
Thuan Nguyen Phuc <thanhthaopl2011@gmail.com>
2026-03-03 22:35:47 +0700

    feat: add ProductItem Component for UI

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { Link } from "react-router-dom";
+
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={Array.isArray(image) ? image[0] : image}
+          alt={name}
+        />
+      </div>
+
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+
+      <p className="text-sm font-medium">
+        {price?.toLocaleString?.()} {currency}
+      </p>
+    </Link>
+  );
+};
+
+export default ProductItem;

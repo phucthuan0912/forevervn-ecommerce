21a4cdd31ecbb77dfec6bf04e68c146c7c5634be
Thuan Nguyen Phuc <thanhthaopl2011@gmail.com>
2026-03-10 22:10:16 +0700

    chore: connect Cloudinary & setup config

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv/config'
-import connectDB from './config/mongodb.js'
-dotenv.config()
-
-const app = express()
-const port = process.env.PORT || 4000
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+dotenv.config();
+// APP config
+const app = express();
+const port = process.env.PORT || 4000;
 // Kết nối MongoDB
-connectDB()
-
+connectDB();
+connectCloudinary();
 
 // Middleware ← Kiểm tra token, quyền truy cập, v.v.`
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 // Các route API sẽ được định nghĩa ở đây routes/
 app.get('/', (req, res) => {
-    res.send('Hello World! API đang chạy...')
-})
+    res.send('Hello World! API đang chạy...');
+});
 // // Kết nối MongoDB
 // mongoose.connect(process.env.MONGODB_URI)
 //     .then(() => console.log('✅ MongoDB đã kết nối'))
 //     .catch((err) => console.log('❌ Lỗi kết nối:', err))
 
 app.listen(process.env.PORT, () => {
-    console.log(`🚀 Server chạy tại port ${process.env.PORT}`)
-})
+    console.log(`🚀 Server chạy tại port ${process.env.PORT}`);
+});

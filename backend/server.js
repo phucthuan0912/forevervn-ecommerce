b1d1d1cc96c28eeaffadbc2c525622c8d68bade9
Thuan Nguyen Phuc <thanhthaopl2011@gmail.com>
2026-03-10 21:59:44 +0700

    chore: connect MongoDB Atlas & setup config

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv/config'
+import connectDB from './config/mongodb.js'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 4000
+// Kết nối MongoDB
+connectDB()
+
+
+// Middleware ← Kiểm tra token, quyền truy cập, v.v.`
+app.use(cors())
+app.use(express.json())
+// Các route API sẽ được định nghĩa ở đây routes/
+app.get('/', (req, res) => {
+    res.send('Hello World! API đang chạy...')
+})
+// // Kết nối MongoDB
+// mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => console.log('✅ MongoDB đã kết nối'))
+//     .catch((err) => console.log('❌ Lỗi kết nối:', err))
+
+app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server chạy tại port ${process.env.PORT}`)
+})

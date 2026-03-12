805119835aee92b5af499b7ecbdb34da451a1eec
Thuan Nguyen Phuc <thanhthaopl2011@gmail.com>
2026-03-12 15:34:43 +0700

    feat: add adminAuth middleware for JWT verification

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { listProducts, addProduct, removeProduct, singleProduct } from '../controllers/productController.js';
-
+import { adminAuth } from '../middleware/authMiddleware.js';
+import upload from '../middleware/multer.js';
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.fields([
+productRouter.post('/add',adminAuth, upload.fields([
     { name: 'image1', maxCount: 1 },
     { name: 'image2', maxCount: 1 },
     { name: 'image3', maxCount: 1 },
     { name: 'image4', maxCount: 1 }
 ]), addProduct);
-productRouter.post('/remove', removeProduct);
-productRouter.post('/single', singleProduct);
-productRouter.get('/list', listProducts);
+productRouter.post('/remove', adminAuth, removeProduct);
+productRouter.post('/single', adminAuth, singleProduct);
+productRouter.get('/list', adminAuth, listProducts);
 
 export default productRouter;
